53ad93b26dd80e5169d441a1adbc72bea3f2a6de
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-07-31 17:32:21 +0200

    process_009

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -4,8 +4,8 @@ import css from "./Notification.module.css";
 const Notification = () => {
   return (
     <div className={css.notification}>
-      <h2>No feedback given yet</h2>
-      <p>Please provide feedback to see the statistics.</p>
+      <h2>No feedback yet</h2>
+      <p>Please provide feedback</p>
     </div>
   );
 };
